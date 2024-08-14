8373c21cfe1279501bdf49c39c07bf452eb5cfbc
Ibrahim Mohamed <ibrahemmohammed11511@gmail.com>
2024-08-14 16:26:16 +0300

    active db.json file as backend and active System page to fetch companies

---
+++ src/Components/company/CompanyBox.jsx
@@ -0,0 +1,4 @@
+/* eslint-disable react/prop-types */
+export default function CompanyBox({ company }) {
+  return <div className="">{company.companyName}</div>;
+}
--- src/Pages/System/System.jsx
+++ src/Pages/System/System.jsx
@@ -1,3 +1,38 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+// /* eslint-disable react-hooks/exhaustive-deps */
+import { useState } from "react";
+import CompanyBox from "../../Components/company/CompanyBox";
+import Styles from "./system.module.css";
+import { useEffect } from "react";
+
 export default function System() {
-  return <div>Hello form system page</div>;
+  const [apiCompaniesResult, setApiCompaniesResult] = useState([]);
+
+  const getCompanies = async () => {
+    fetch("http://localhost:3000/companies")
+      .then((res) => res.json())
+      .then((data) => {
+        setApiCompaniesResult(data || []);
+      });
+  };
+
+  useEffect(() => {
+    getCompanies();
+  }, []);
+
+  return (
+    <main className={Styles.systemPage}>
+      <section className={Styles.Companies}>
+        <div className={Styles.newCompany}>
+          <input type="text" placeholder="Enter Company name" />
+          <button className={Styles.addCompanyBtn}>ADD</button>
+        </div>
+        <div className={Styles.currentCompaniesContainer}>
+          {apiCompaniesResult.map((company) => (
+            <CompanyBox key={company.id} company={company} />
+          ))}
+        </div>
+      </section>
+    </main>
+  );
 }
