01a4320960746dc04387ab9319334e86eae34e4e
Ibrahim11511 <109541964+Ibrahim11511@users.noreply.github.com>
2024-08-15 04:36:27 +0300

    get all users names from api backend make some changes

--- src/Components/company/CompanyBox.jsx
+++
@@ -1,4 +0,0 @@
-/* eslint-disable react/prop-types */
-export default function CompanyBox({ company }) {
-  return <div className="">{company.companyName}</div>;
-}
--- src/Pages/System/System.jsx
+++ src/Pages/System/System.jsx
@@ -1,35 +1,70 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-// /* eslint-disable react-hooks/exhaustive-deps */
 import { useState } from "react";
-import CompanyBox from "../../Components/company/CompanyBox";
+import NameBox from "../../Components/Name Box/NameBox";
 import Styles from "./system.module.css";
 import { useEffect } from "react";
 
 export default function System() {
   const [apiCompaniesResult, setApiCompaniesResult] = useState([]);
+  const [apiUsersResult, setApiUsersResult] = useState([]);
+  const [newCompaniesInput, setNewCompaniesInput] = useState("");
 
   const getCompanies = async () => {
     fetch("http://localhost:3000/companies")
       .then((res) => res.json())
       .then((data) => {
-        setApiCompaniesResult(data || []);
+        data.map((company) => {
+          setApiCompaniesResult((prev) => [...prev, company.companyName]);
+        });
+      });
+  };
+
+  const getUsers = async () => {
+    fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((users) => {
+        users.map((user) => {
+          setApiUsersResult((prev) => [...prev, user.name]);
+        });
       });
   };
 
   useEffect(() => {
     getCompanies();
+    getUsers();
   }, []);
+
+  console.log(apiUsersResult);
 
   return (
     <main className={Styles.systemPage}>
       <section className={Styles.Companies}>
         <div className={Styles.newCompany}>
-          <input type="text" placeholder="Enter Company name" />
+          <input
+            type="text"
+            placeholder="Add New Company"
+            value={newCompaniesInput}
+            onChange={(e) => setNewCompaniesInput(e.target.value)}
+          />
           <button className={Styles.addCompanyBtn}>ADD</button>
         </div>
         <div className={Styles.currentCompaniesContainer}>
           {apiCompaniesResult.map((company) => (
-            <CompanyBox key={company.id} company={company} />
+            <NameBox key={`company-name-${company}`} Names={company} />
+          ))}
+        </div>
+      </section>
+      <section className={Styles.Users}>
+        <div className={Styles.newUser}>
+          <input type="text" placeholder="User Name" />
+          <input type="text" placeholder="Password" />
+          <input type="text" placeholder="Name" />
+          <label htmlFor="admin">admin</label>
+          <input type="checkbox" />
+          <button className={Styles.addUserBtn}>ADD</button>
+        </div>
+        <div className={Styles.currentUser}>
+          {apiUsersResult.map((user) => (
+            <NameBox key={`company-name-${user}`} Names={user} />
           ))}
         </div>
       </section>
